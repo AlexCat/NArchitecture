93547853321e65d1863f7f2c8f6bdc8ca0a4b902
Premysl Krajcovic <premyslkrajcovic@gmail.com>
2016-10-19 21:18:57 +0200

    Add nuspec for building nuget package

--- NArchitecture/Properties/AssemblyInfo.cs
+++ NArchitecture/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NArchitecture")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Architectural patterns for business layer.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Premysl Krajcovic")]
 [assembly: AssemblyProduct("NArchitecture")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
