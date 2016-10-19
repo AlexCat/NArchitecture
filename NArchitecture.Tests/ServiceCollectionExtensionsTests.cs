958c9f6b28665a64d4a8736cd79589e0fa59ecd6
Premysl Krajcovic <premyslkrajcovic@gmail.com>
2016-10-19 19:48:20 +0200

    Improve coverage

--- NArchitecture.Tests/ServiceCollectionExtensionsTests.cs
+++ NArchitecture.Tests/ServiceCollectionExtensionsTests.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Xunit;
+using System;
+using System.Threading.Tasks;
 
 namespace NArchitecture.Tests
 {
@@ -14,29 +16,59 @@ namespace NArchitecture.Tests
             Assert.NotNull(provider.GetService<IBus>());
         }
 
+        public class EventHandler : IEventHandler
+        {
+            public Task Handle(EventHandlerContext context, IEvent @event)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
         [Fact(DisplayName = "ServiceCollectionExtensions can add EventService")]
         public void AddEventServiceTest()
         {
             var services = new ServiceCollection();
-            services.AddEventService(options => { });
+            services.AddEventService(options => { options.AddEventHandler<EventHandler>(); });
             var provider = services.BuildServiceProvider();
             Assert.NotNull(provider.GetService<IEventService>());
         }
+
+        public class RequestHandler : IRequestHandler
+        {
+            public bool CanHandle(IRequest request)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task Handle(RequestHandlerContext context, IRequest request)
+            {
+                throw new NotImplementedException();
+            }
+        }
+
 
         [Fact(DisplayName = "ServiceCollectionExtensions can add RequestService")]
         public void AddRequestServiceTest()
         {
             var services = new ServiceCollection();
-            services.AddRequestService(options => { });
+            services.AddRequestService(options => { options.AddRequestHandler<RequestHandler>(); });
             var provider = services.BuildServiceProvider();
             Assert.NotNull(provider.GetService<IRequestService>());
+        }
+
+        public class AuthorizationHandler : IAuthorizationHandler
+        {
+            public Task Handle(AuthorizationHandlerContext context)
+            {
+                throw new NotImplementedException();
+            }
         }
 
         [Fact(DisplayName = "ServiceCollectionExtensions can add AuthorizationService")]
         public void AddAuthorizationServiceTest()
         {
             var services = new ServiceCollection();
-            services.AddAuthorizationService(options => { });
+            services.AddAuthorizationService(options => { options.AddAuthorizationHandler<AuthorizationHandler>(); });
             var provider = services.BuildServiceProvider();
             Assert.NotNull(provider.GetService<IAuthorizationService>());
         }
